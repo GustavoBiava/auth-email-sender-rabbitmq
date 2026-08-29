bebc831c5aacf0eee77950bd80be9dc1e427c303
Gustavo Biava <gustavo.rbiava@hotmail.com>
2026-08-29 01:19:35 -0300

    fix: adjusting UsernamePasswordAuthenticationToken object instancing in /login route

--- auth-service/src/main/java/dev/biava/auth/controllers/AuthenticationController.java
+++ auth-service/src/main/java/dev/biava/auth/controllers/AuthenticationController.java
@@ -22,29 +22,29 @@ public class AuthenticationController {
     
     @Autowired
     private AuthenticationManager authenticationManager;
-
+    
     @Autowired
     private UserRepository userRepository;
-
+    
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Validated UserLoginDTO userLoginDTO) {
-        var usernamePassword = UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password());
+        var usernamePassword = new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password());
         var auth = authenticationManager.authenticate(usernamePassword);
-
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Validated UserRegisterDTO userRegisterDTO) {
-        if (this.userRepository.findByEmail(userRegisterDTO.email()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(userRegisterDTO.password());
-        var user = new User(userRegisterDTO.username(), userRegisterDTO.email(), encryptedPassword, userRegisterDTO.role());
-
-        this.userRepository.save(user);
-
+        
         return ResponseEntity.ok().build();
     }
     
-
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Validated UserRegisterDTO userRegisterDTO) {
+        if (this.userRepository.findByEmail(userRegisterDTO.email()) != null) return ResponseEntity.badRequest().build();
+        
+        String encryptedPassword = new BCryptPasswordEncoder().encode(userRegisterDTO.password());
+        var user = new User(userRegisterDTO.username(), userRegisterDTO.email(), encryptedPassword, userRegisterDTO.role());
+        
+        this.userRepository.save(user);
+        
+        return ResponseEntity.ok().build();
+    }
+    
+    
 }
