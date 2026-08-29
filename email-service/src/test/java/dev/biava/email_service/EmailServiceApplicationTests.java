625dde2ebffaec9fcbf7900d11da9280f5291e4a
Gustavo Biava <gustavo.rbiava@hotmail.com>
2026-08-29 17:25:12 -0300

    chore: email service structure

---
+++ email-service/src/test/java/dev/biava/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.biava.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
