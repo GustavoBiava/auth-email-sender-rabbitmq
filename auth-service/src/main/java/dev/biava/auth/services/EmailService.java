fa6f8650d318201cd443440992d375b2700b56e7
Gustavo Biava <gustavo.rbiava@hotmail.com>
2026-08-30 16:49:24 -0300

    feat: EmailService to publish event in Producer

---
+++ auth-service/src/main/java/dev/biava/auth/services/EmailService.java
@@ -0,0 +1,24 @@
+package dev.biava.auth.services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import dev.biava.auth.domain.Email.EmailDTO;
+
+@Service
+public class EmailService {
+    
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${app.rabbitmq.exchange.email}")
+    private String emailExchange;
+
+    @Value("${app.rabbitmq.routingkey.email}")
+    private String emailRoutingKey;
+
+    public void publishEmailEvent(EmailDTO emailDTO) {
+        rabbitTemplate.convertAndSend(this.emailExchange, this.emailRoutingKey, emailDTO);
+    }
+
+}
